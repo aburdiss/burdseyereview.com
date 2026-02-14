a863badfc55c2c9c2237cdcb0bef4ac7ea967a86
Alexander Burdiss <aburdiss@icloud.com>
2026-02-14 11:50:48 -0500

    feat: update homepage content

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -40,9 +40,8 @@ export default function Home() {
         <h2>Come See the world from a Burd's eye view.</h2>
         <p>
           This is a site where we will post musings and reviews of things in our
-          lives. Subscribe to our email newsletter to hear what we've been up to
-          and get music recommendations, recipes, and learn more about cool
-          things in the Dayton Area!
+          lives. We'll start up an email newsletter when we feel like doing this
+          more consistently!
         </p>
       </hgroup>
       <section>
